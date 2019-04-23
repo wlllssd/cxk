2473b6dd5c5d4a072fc68ba47ffefbc84f5dddfc
wlllssd <511872591@qq.com>
2019-04-24 01:37:48 +0800

    init

---
+++ test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import time
+import math
+import os
+import sys
+import os, os.path,shutil
+print(os.getcwd())
+txtPath = os.getcwd()+'\\data_txt'
+print(txtPath)
+txtLists = os.listdir(txtPath)
+print(txtLists)
+
+for txt in txtLists:
+	with open(txtPath+'\\'+txt,'r',encoding='utf-8') as t:
+		lines = t.readlines()
+		for line in lines:
+			print(line[10:-10],end='')
+		print()
+	time.sleep(0.5)
+	os.system('cls')
